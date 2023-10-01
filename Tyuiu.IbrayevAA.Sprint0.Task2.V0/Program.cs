8e980e0724e1da59228201f5e8ae9884408b9a98
Tyuiu.IbrayevAA@outlook.com <03.05.05.ibraev.abai@gmail.com>
2023-10-01 21:44:25 +0500

    Task2

---
+++ Tyuiu.IbrayevAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IbrayevAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.IbrayevAA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine(DataService.GetMessage("Абай"));
+            Console.ReadKey();
+        }
+    }
+}
